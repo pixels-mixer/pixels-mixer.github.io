93c349d8b6a5e56788eff7df86ef6331833c84f8
mandriic <kellaxxx@gmail.com>
2025-04-10 20:33:33 +0200

    fixed ver 0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
-module.exports = {
-    content: ["./**/*.html"],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['Doto', 'sans-serif'],         // основний текст
-          heading: ['Raleway', 'sans-serif'],   // заголовки
-        },
-      },
-    },
-    plugins: [],
-  }
+// module.exports = {
+//     content: [
+//         "./index.html",
+//         "./src/**/*.{html,js}", // Укажи шлях до своїх файлів
+//       ],
+//     theme: {
+//       extend: {
+//         fontFamily: {
+//           display: ['Bebas Neue', 'sans-serif'],
+//           body: ['Manrope', 'sans-serif'],
+//           serif: ['DM Serif Display', 'serif'],
+//           logo: ['Monoton', 'cursive'],
+//           shadow: ['Londrina Shadow', 'cursive'],
+//           doted: ['Doto', 'sans-serif'],
+//         },
+//       },
+//     },
+//   }
   
